ce04a170eedc96ff51b6285164211f64b44e4847
MrLYC <imyikong@gmail.com>
2026-04-30 18:07:07 +0800

    feat(common): export hybrid scan pipeline API
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- upcast/common/__init__.py
+++ upcast/common/__init__.py
@@ -12,15 +12,36 @@ from upcast.common.astroid_matcher import MatchResult, find_matches, match
 from upcast.common.cst_ast_mapper import CSTASTMapper
 from upcast.common.export import export_to_json, export_to_yaml, sort_dict_recursive
 from upcast.common.file_utils import collect_python_files, find_package_root, validate_path
+from upcast.common.hybrid_scan_pipeline import (
+    Finding,
+    LocateStage,
+    MapStage,
+    PipelineRunResult,
+    PipelineSpec,
+    ProjectStage,
+    SemanticDecision,
+    SemanticFilterStage,
+    StructuralCandidate,
+    run_pipeline,
+)
 from upcast.common.inference import InferenceResult, StringPattern, infer_string_pattern, infer_type, infer_value
 from upcast.common.patterns import DEFAULT_EXCLUDES, match_patterns, should_exclude
 
 __all__ = [
     "DEFAULT_EXCLUDES",
     "CSTASTMapper",
+    "Finding",
     "InferenceResult",
+    "LocateStage",
+    "MapStage",
     "MatchResult",
+    "PipelineRunResult",
+    "PipelineSpec",
+    "ProjectStage",
+    "SemanticDecision",
+    "SemanticFilterStage",
     "StringPattern",
+    "StructuralCandidate",
     "collect_python_files",
     "export_to_json",
     "export_to_yaml",
@@ -32,6 +53,7 @@ __all__ = [
     "infer_value",
     "match",
     "match_patterns",
+    "run_pipeline",
     "safe_as_string",
     "should_exclude",
     "sort_dict_recursive",
